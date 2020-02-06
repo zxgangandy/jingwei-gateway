14184b1f59b7134be081c6fd0aea82d67fa3f82d
zhaoxiaogang <Zxg123456>
2020-02-07 00:43:05 +0800

    init version;

---
+++ src/main/java/io/jingwei/gateway/GatewayApplication.java
@@ -0,0 +1,14 @@
+package io.jingwei.gateway;
+
+import com.ctrip.framework.apollo.spring.annotation.EnableApolloConfig;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableApolloConfig
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
